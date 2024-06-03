55acdd15290dcaaa6d03f6d6aaab754f03bb8809
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2024-06-03 13:34:22 +0700

    nix: update nixpkgs version to 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Abbit's Nix configuration flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     nix-darwin = {
@@ -11,7 +11,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
