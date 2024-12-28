dceb20337e47d018d6af7b14ae373da552ed14bb
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2024-12-28 13:16:39 +0700

    nvim: add terraform lsp

--- configs/nvim/lua/plugins/lsp.lua
+++ configs/nvim/lua/plugins/lsp.lua
@@ -145,6 +145,7 @@ return {
           clangd = {
             filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
           },
+          terraformls = {},
         },
         -- you can do any additional lsp server setup here
         -- return true if you don't want this server to be setup with lspconfig
